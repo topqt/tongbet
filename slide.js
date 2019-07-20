4b33ff7cfc60d1056b3b9138753119573353aebd
topqt <KaydenPQThang@gmail.com>
2019-07-21 02:26:48 +0700

    Thang

---
+++ slide.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    var stt = 0;
+    $("img.slide").each(function(
+        if ($(this).is(':visible')) 
+            stt = $(this).attr("stt");
+    ));
+    setInterval(function(){
+        
+    }
+}
